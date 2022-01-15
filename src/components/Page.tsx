d3fb4ddd58ceeb4e2ebc919fca67f08a6d2529e0
Billy Huang <10646003@ntub.edu.tw>
2022-01-16 05:22:17 +0800

    feat(CH11): nested Route

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Link, useLocation } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 
 const Whoops404 = () => {
   const location = useLocation();
@@ -14,6 +14,9 @@ const Home = () => {
       <h1>[Company Website]</h1>
       <nav>
         <Link to='about'>About</Link>
+        <Link to='about/services'>Services</Link>
+        <Link to='about/history'>History</Link>
+        <Link to='about/location'>Location</Link>
         <Link to='event'>Event</Link>
         <Link to='product'>Product</Link>
         <Link to='contact'>Contact</Link>
@@ -24,7 +27,28 @@ const Home = () => {
 };
 
 const About = () => {
-  return <h1>[About]</h1>;
+  return (
+    <>
+      <h1>[About]</h1>
+      <Outlet />
+    </>
+  );
+};
+
+const Company = () => {
+  return <h2>Our Company</h2>;
+};
+
+const History = () => {
+  return <h2>Our History</h2>;
+};
+
+const Services = () => {
+  return <h2>Our Services</h2>;
+};
+
+const Location = () => {
+  return <h2>Our Location</h2>;
 };
 
 const Event = () => {
@@ -39,4 +63,15 @@ const Contact = () => {
   return <h1>[Contact]</h1>;
 };
 
-export { Whoops404, Home, About, Event, Product, Contact };
+export {
+  Whoops404,
+  Home,
+  About,
+  Company,
+  History,
+  Services,
+  Location,
+  Event,
+  Product,
+  Contact,
+};
--- src/components/RouterApp.tsx
+++ src/components/RouterApp.tsx
@@ -2,16 +2,32 @@ import React from 'react';
 
 import { Route, Routes } from 'react-router';
 
-import { About, Contact, Event, Home, Product, Whoops404 } from './Page';
+import {
+  About,
+  Company,
+  Contact,
+  Event,
+  History,
+  Home,
+  Location,
+  Product,
+  Services,
+  Whoops404,
+} from './Page';
 
 const RouterApp = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/event' element={<Event />} />
-      <Route path='/product' element={<Product />} />
-      <Route path='/contact' element={<Contact />} />
+      <Route path='about' element={<About />}>
+        <Route path='' element={<Company />} />
+        <Route path='services' element={<Services />} />
+        <Route path='history' element={<History />} />
+        <Route path='location' element={<Location />} />
+      </Route>
+      <Route path='event' element={<Event />} />
+      <Route path='product' element={<Product />} />
+      <Route path='contact' element={<Contact />} />
       <Route path='*' element={<Whoops404 />} />
     </Routes>
   );
