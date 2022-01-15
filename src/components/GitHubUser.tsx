e7add962b80b50a27e6e16fe82c6d6b56fd80aac
Billy Huang <10646003@ntub.edu.tw>
2022-01-15 18:47:12 +0800

    feat(CH8): save data at local

--- src/components/GitHubUser.tsx
+++ src/components/GitHubUser.tsx
@@ -4,8 +4,25 @@ type GitHubUserProp = {
   login: string;
 };
 
+const loadJson = (key: string) =>
+  JSON.parse(localStorage.getItem(key) || 'null');
+const saveJson = (key: string, data: any) =>
+  localStorage.setItem(key, JSON.stringify(data));
+
 const GitHubUser = ({ login }: GitHubUserProp) => {
-  const [data, setData] = useState();
+  const [data, setData] = useState(loadJson(`user:${login}`));
+
+  useEffect(() => {
+    if (!data) return;
+    if (data.login === login) return;
+    const { name, avatar_url, location } = data;
+    saveJson(`user:${login}`, {
+      login,
+      name,
+      avatar_url,
+      location,
+    });
+  }, [data]);
 
   useEffect(() => {
     if (!login) return;
