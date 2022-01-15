52d6971278bedb076606d4f9851bdf42b206dada
Billy Huang <10646003@ntub.edu.tw>
2022-01-16 03:31:56 +0800

    feat(CH10): throw Promise object

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
 const loadStatus = () => {
-  throw new Promise((resolves) => null);
+  console.log('load status');
+  // throw new Promise((resolves) => setTimeout(resolves, 3000));
+  return 'success - ready';
 };
+
+// const safe = (fn: () => void) => {
+//   try {
+//     fn();
+//   } catch (error) {
+//     if (error instanceof Promise) {
+//       error.then(() => safe(fn));
+//     } else {
+//       throw error;
+//     }
+//   }
+// };
 
 const Status = () => {
   const status = loadStatus();
