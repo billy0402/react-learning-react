eabfbd5387ded2d8543a212843ae513acef4dd82
Billy Huang <10646003@ntub.edu.tw>
2022-01-16 04:12:30 +0800

    feat(CH10): create Suspense style data resource

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
-const loadStatus = () => {
-  console.log('load status');
-  // throw new Promise((resolves) => setTimeout(resolves, 3000));
-  return 'success - ready';
+type Gnar = {
+  gnar: string;
 };
+
+const createResource = <T,>(promise: Promise<T>) => {
+  let response: T, error: Error;
+  promise.then((r) => (response = r)).catch((e) => (error = e));
+
+  return {
+    read(): T {
+      if (error) throw error;
+      if (response) return response;
+      throw promise;
+    },
+  };
+};
+
+const threeSecondsToGnar = new Promise<Gnar>((resolves) =>
+  setTimeout(() => resolves({ gnar: 'gnarly!' }), 3000),
+);
 
 // const safe = (fn: () => void) => {
 //   try {
@@ -18,9 +33,11 @@ const loadStatus = () => {
 //   }
 // };
 
+const resource = createResource<Gnar>(threeSecondsToGnar);
+
 const Status = () => {
-  const status = loadStatus();
-  return <h1>status: {status}</h1>;
+  const result = resource.read();
+  return <h1>Gnar: {result.gnar}</h1>;
 };
 
 export default Status;
